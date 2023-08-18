9993b0c7fc08a6a713c508735c76e1de302bb4fc
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-08-18 11:42:39 +0700

    diff between template and layout

---
+++ src/app/layout/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { PropsWithChildren, useState } from "react";
+
+export default function Layout({ children }: PropsWithChildren) {
+  const [count, setCount] = useState(0);
+  console.log("layout rerender");
+  return (
+    <div>
+      <Link
+        className="bg-white text-black px-4 py-2 rounded-sm mr-2"
+        href="/layout/hello"
+      >
+        Hello
+      </Link>
+      <Link
+        className="bg-white text-black px-4 py-2 rounded-sm"
+        href="/layout/world"
+      >
+        World
+      </Link>
+      <button
+        className="block mx-2 my-2 border-white border rounded-sm"
+        onClick={() => setCount(count + 1)}
+      >
+        Clicked! {count}
+        {` time${count > 1 ? "s" : ""}`}
+      </button>
+      {children}
+    </div>
+  );
+}
